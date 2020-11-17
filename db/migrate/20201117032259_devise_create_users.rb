b85602e7e5aca77d153099ca371d96b923145d9b
fukuda0921 <mitsu0921.fukuda@i.softbank.jp>
2020-11-17 14:39:06 +0900

    カラムの追加

--- db/migrate/20201117032259_devise_create_users.rb
+++ db/migrate/20201117032259_devise_create_users.rb
@@ -6,6 +6,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
+      t.string :nickname
+      t.integer :prefecture_id
+      t.integer :period_id
+      t.string :target
 
       ## Recoverable
       t.string   :reset_password_token
--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,10 @@ ActiveRecord::Schema.define(version: 2020_11_17_032259) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "nickname"
+    t.integer "prefecture_id"
+    t.integer "period_id"
+    t.string "target"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
