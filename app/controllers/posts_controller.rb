f7c226ab7f9c1ebb040bd74a185814f3c4e4d2bc
fukuda0921 <mitsu0921.fukuda@i.softbank.jp>
2020-11-21 03:09:25 +0900

    投稿ページの作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,17 @@ class PostsController < ApplicationController
   def index
   end
   def new
+    @post = Post.new
   end
   def create
+    binding.pry
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    end
+  end
+  private
+  def post_params
+    params.require(:post).permit(:image, :title, :prefecture_id, :budget_id, :explain, :transportation_id, :place).merge(user_id: current_user.id)
   end
 end
