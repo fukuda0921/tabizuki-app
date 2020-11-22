b75b19455b2d3ca7701a77449d02308cee955111
fukuda0921 <mitsu0921.fukuda@i.softbank.jp>
2020-11-23 08:34:52 +0900

    viewの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "top#index"
-  resources :posts, only: [:index, :new, :create]
-  resources :users, only: [:show]
+  resources :posts, only: [:index, :new, :create] 
+   resources :users, only: [:show]
 end
