eac9374ac71de24b3e1b9ee5677ee9507e89a96b
fukuda0921 <mitsu0921.fukuda@i.softbank.jp>
2020-11-26 04:39:22 +0900

    投稿詳細ページveiw作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users,controllers: {   registrations: 'users/registrations' }
   root to: "top#index"
-  resources :posts, only: [:index, :new, :create] 
+  resources :posts, only: [:index, :new, :create, :show] 
    resources :users, only: [:show]
 end
