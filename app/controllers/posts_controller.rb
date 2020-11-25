1ce87d65463e053294e97d9885727499ce09fce8
fukuda0921 <mitsu0921.fukuda@i.softbank.jp>
2020-11-26 05:33:58 +0900

    編集、削除機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
-  before_action :authenticate_user!, only: [:index, :new]
+  before_action :authenticate_user!, only: [:index, :new,:edit, :destory]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
   def index
     @posts = Post.order('created_at DESC')
   end
@@ -16,10 +17,31 @@ class PostsController < ApplicationController
   end
 
   def show 
-    @post = Post.find(params[:id])
+  end
+  
+  def edit
+    unless current_user.id == @post.user_id
+      redirect_to action: :index
+    end
+  end
+
+  def update
+    if @post.update(post_params)
+      redirect_to post_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy if current_user.id == @post.user_id
+    redirect_to posts_path
   end
 
   private
+  def set_post
+    @post = Post.find(params[:id])
+  end
   def post_params
     params.require(:post).permit(:image, :title, :prefecture_id, :budget_id, :explain, :transportation_id, :place).merge(user_id: current_user.id)
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users,controllers: {   registrations: 'users/registrations' }
   root to: "top#index"
-  resources :posts, only: [:index, :new, :create, :show] 
+  resources :posts 
    resources :users, only: [:show]
 end
