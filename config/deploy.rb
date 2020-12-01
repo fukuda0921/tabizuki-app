8d70ab5980f5dd79d9ae03e59efd113dca91ea75
fukuda0921 <mitsu0921.fukuda@i.softbank.jp>
2020-12-02 02:23:29 +0900

    自動デプロイ設定

---
+++ config/deploy.rb
@@ -0,0 +1,32 @@
+lock '3.14.1'
+
+
+set :application, 'tabizuki-app'
+
+
+set :repo_url,  'git@github.com:fukuda0921/tabizuki-app.git'
+
+
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
+
+set :rbenv_type, :user
+set :rbenv_ruby, '2.6.5' 
+
+
+set :ssh_options, auth_methods: ['publickey'],
+                                  keys: ['~/.ssh/fukuda0921.pem'] 
+
+
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
+
+
+set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
+set :keep_releases, 5
+
+
+after 'deploy:publishing', 'deploy:restart'
+namespace :deploy do
+  task :restart do
+    invoke 'unicorn:restart'
+  end
+end
